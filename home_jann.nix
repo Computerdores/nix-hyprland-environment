ebe227bd78996c2fec38b6bb340e344815bcb2b9
Jann Stute <jann.stute@protonmail.com>
2024-10-20 18:02:03 +0200

    add some more binds

--- home_jann.nix
+++ home_jann.nix
@@ -16,16 +16,34 @@
 
             settings = {
                 "$terminal" = "kitty";
+                "$fileManager" = "nnn";
+                "$menu" = "wofi --show drun";
                 "$mainMod" = "SUPER";
                 bind = [
                     "$mainMod, Q, exec, $terminal"
                     "$mainMod, C, killactive"
                     "$mainMod, M, exit"
+                    "$mainMod, E, exec, $fileManager"
+                    "$mainMod, V, togglefloating"
+                    "$mainMod, R, exec, $menu"
+                    "$mainMod, P, pseudo"       # dwindle
+                    "$mainMod, J, togglesplit"  # dwindle
+
+                    "$mainMod, left, movefocus, l"
+                    "$mainMod, right, movefocus, r"
+                    "$mainMod, up, movefocus, u"
+                    "$mainMod, down, movefocus, d"
+                    
+                    #todo
                 ];
+                
                 input = {
                     kb_layout = "de";
                     kb_model = "pc105";
+                    follow_mouse = 1;
+                    touchpad.natural_scroll = true;
                 };
+
                 monitor = [
                     ",1920x1080@60,0x0,1"
                 ];
