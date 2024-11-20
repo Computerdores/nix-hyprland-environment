fbe9cfb65bd1f8f3f5bb55808c4c180d36b27ada
Jann Stute <jann.stute@protonmail.com>
2024-11-20 17:04:08 +0100

    add missing fonts for eww

--- hosts/nixos-testbed/configuration.nix
+++ hosts/nixos-testbed/configuration.nix
@@ -70,6 +70,11 @@
         vivaldi
     ];
 
+    fonts.packages = with pkgs; [
+        font-awesome
+        noto-fonts
+    ];
+
     # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
     system.stateVersion = "24.05"; # don't touch
 }
