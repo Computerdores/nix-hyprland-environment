75455d29c2f61697f1471443ce7f3ba10978f26d
Jann Stute <jann.stute@protonmail.com>
2024-12-17 22:07:15 +0100

    upgrade to 24.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
     description = "Test for a NixOS System with Home Manager via a flake, with everything in a single repo.";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+        nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
         home-manager = {
-            url = "github:nix-community/home-manager/release-24.05";
+            url = "github:nix-community/home-manager/release-24.11";
             inputs.nixpkgs.follows = "nixpkgs";
         };
         hyprland.url = "github:hyprwm/Hyprland";
