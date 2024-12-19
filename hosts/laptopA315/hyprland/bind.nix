1afbcad51fbc66182d407fc70e43ee572296b62c
Jann Stute <jann.stute@protonmail.com>
2024-12-19 21:05:12 +0100

    fix movetoworkspace keybinds

--- hosts/laptopA315/hyprland/bind.nix
+++ hosts/laptopA315/hyprland/bind.nix
@@ -29,16 +29,16 @@
     "$mainMod, 0, workspace, 10"
 
     # Move active window to a workspace with mainMod + SHIFT + [0-9]
-    "$mainMod, 1, movetoworkspace, 1"
-    "$mainMod, 2, movetoworkspace, 2"
-    "$mainMod, 3, movetoworkspace, 3"
-    "$mainMod, 4, movetoworkspace, 4"
-    "$mainMod, 5, movetoworkspace, 5"
-    "$mainMod, 6, movetoworkspace, 6"
-    "$mainMod, 7, movetoworkspace, 7"
-    "$mainMod, 8, movetoworkspace, 8"
-    "$mainMod, 9, movetoworkspace, 9"
-    "$mainMod, 0, movetoworkspace, 10"
+    "$mainMod SHIFT, 1, movetoworkspace, 1"
+    "$mainMod SHIFT, 2, movetoworkspace, 2"
+    "$mainMod SHIFT, 3, movetoworkspace, 3"
+    "$mainMod SHIFT, 4, movetoworkspace, 4"
+    "$mainMod SHIFT, 5, movetoworkspace, 5"
+    "$mainMod SHIFT, 6, movetoworkspace, 6"
+    "$mainMod SHIFT, 7, movetoworkspace, 7"
+    "$mainMod SHIFT, 8, movetoworkspace, 8"
+    "$mainMod SHIFT, 9, movetoworkspace, 9"
+    "$mainMod SHIFT, 0, movetoworkspace, 10"
 
     # Scratch Workspace
     "$mainMod, S, togglespecialworkspace, magic"
