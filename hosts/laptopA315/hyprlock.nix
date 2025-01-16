0cc27a97503a0a48c698baf72eeac8f93fe313f2
Jann Stute <jann.stute@protonmail.com>
2025-01-16 22:51:25 +0100

    improve hyprlock config

--- hosts/laptopA315/hyprlock.nix
+++ hosts/laptopA315/hyprlock.nix
@@ -17,9 +17,15 @@
         placeholder_text = "Password...";
         halign = "center";
         valign = "center";
+        fade_on_empty = false;
+        inner_color = "rgba(49,50,68,0.5)";
+        outer_color = "rgba(69,71,90,0.5)";
+        check_color = "rgba(20,10,100,1)";
+        capslock_color = "rgba(204,136,34,1)";
+        font_color = "rgba(200,200,200,1)";
     };
     label = {
-        text = "Test123, $USER";
+        text = "Hallo, $USER";
         position = "0, 80";
         halign = "center";
         valign = "center";
