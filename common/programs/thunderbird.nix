ed702c30d54f9712d3fe65535ffceeeaf7412f35
Jann Stute <jann.stute@protonmail.com>
2026-02-02 17:43:13 +0100

    feat: reformat + expand thunderbird settings

--- common/programs/thunderbird.nix
+++ common/programs/thunderbird.nix
@@ -5,13 +5,33 @@
         enable = true;
         package = (lib.gtkEnablePortals pkgs pkgs.thunderbird);
         preferences = {
-            "mail.tabs.drawInTitlebar"    = false; # hide gnome exit button in titlebar
-            "mail.tabs.autoHide"          = false;
+            # General
+            ## Thunderbird Start Page
+            "mailnews.start_page.enabled"          = false;
+                                                   
+            # Appearance                           
+            ## Window Layout                       
+            "mail.tabs.drawInTitlebar"             = false; # hide gnome exit button in titlebar
+            "mail.tabs.autoHide"                   = false;
+                                                   
+            ## Sorting and Threading               
+            ### Default Threading:                 
+            "mailnews.default_view_flags"          =  1;
+                                                   
+            ### Default Sort by:                   
+            "mailnews.default_sort_type"           = 18;
+                                                   
+            ### Default Sort Order:                
+            "mailnews.default_sort_order"          =  1;
 
-            "mailnews.start_page.enabled" = false;
-            "mailnews.default_sort_type"  = 18;
-            "mailnews.default_sort_order" =  1;
-            "security.default_personal_cert" = "Select Automatically";
+            # Privacy & Security
+            ## Privacy
+            ### Web Content
+            "privacy.globalprivacycontrol.enabled" = true;
+
+            ## Security
+            ### Certificates
+            "security.default_personal_cert"       = "Select Automatically";
         };
     };
 }
