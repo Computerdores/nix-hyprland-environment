02660bb788ca7a469afe310da677d63a85358921
Jann Stute <jann.stute@protonmail.com>
2026-02-03 00:17:27 +0100

    program(thunderbird): expand settings

--- common/programs/thunderbird.nix
+++ common/programs/thunderbird.nix
@@ -5,13 +5,19 @@
         enable = true;
         package = (lib.gtkEnablePortals pkgs pkgs.thunderbird);
         preferences = {
-            "mail.tabs.drawInTitlebar"    = false; # hide gnome exit button in titlebar
-            "mail.tabs.autoHide"          = false;
+            "browser.search.region"              = "DE";
 
-            "mailnews.start_page.enabled" = false;
-            "mailnews.default_sort_type"  = 18;
-            "mailnews.default_sort_order" =  1;
-            "security.default_personal_cert" = "Select Automatically";
+            "mail.tabs.drawInTitlebar"           = false; # hide gnome exit button in titlebar
+            "mail.tabs.autoHide"                 = false;
+
+            "mailnews.start_page.enabled"        = false;
+            "mailnews.default_sort_type"         = 18;
+            "mailnews.default_sort_order"        =  1;
+
+            "intl.regional_prefs.use_os_locales" = true;
+            "calendar.week.start"                = 1;
+
+            "security.default_personal_cert"     = "Select Automatically";
         };
     };
 }
