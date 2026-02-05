8759c2ba39e7bc737c64805ab7b710c2dcc361a9
Jann Stute <jann.stute@protonmail.com>
2026-02-05 18:39:53 +0100

    program(tower): signal + telegram

--- hosts/tower/configuration.nix
+++ hosts/tower/configuration.nix
@@ -124,6 +124,8 @@ args@{ inputs, config, lib, pkgs, hyprland-pkgs, flakeDir, system, username, ...
         wiremix
         (import ../../common/derivations/sleep-inhibit.nix args)
         (lib.gtkEnablePortals pkgs pkgs.localsend)
+        telegram-desktop
+        signal-desktop
     ];
 
     fonts.packages = with pkgs; [
