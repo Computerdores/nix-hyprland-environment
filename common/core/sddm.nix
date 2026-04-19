da085bff1b0f9a2560e959b300f41b858ce9e072
Jann Stute <jann.stute@protonmail.com>
2026-04-19 14:32:21 +0200

    fix: washed out colors bug was still present on SDDM

--- common/core/sddm.nix
+++ common/core/sddm.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 let
     wayland = false; # enabling wayland currently leads to a bug when logging in too early
@@ -19,5 +19,17 @@ in {
         package = pkgs.kdePackages.sddm;
         theme = "astronaut";
     };
-    services.xserver.enable = !wayland;
+
+    services.xserver = {
+        enable = !wayland;
+
+        config = lib.mkAfter ''
+            # Fixes an issue with washed out colors because of the Intel GPU
+            Section "Monitor"
+                Identifier "HDMI-2"
+                Modeline "1920x1080_58.00"  166.00  1920 2040 2240 2560  1080 1083 1088 1119 -hsync +vsync
+                Option "PreferredMode" "1920x1080_58.00"
+            EndSection
+        '';
+    };
 }
