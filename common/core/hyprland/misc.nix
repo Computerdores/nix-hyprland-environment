919806cf7005d2cff8b40888ac5119730f8159a0
Jann Stute <jann.stute@protonmail.com>
2026-04-26 16:04:59 +0200

    fix: enable inital_workspace_tracking to always launch child windows on same workspace as parent

--- common/core/hyprland/misc.nix
+++ common/core/hyprland/misc.nix
@@ -1,4 +1,5 @@
 # see https://wiki.hyprland.org/Configuring/Variables for more
 {
     force_default_wallpaper = 0; # set to 0 or 1 to disable anime mascot wallpapers
+    initial_workspace_tracking = 2;
 }
