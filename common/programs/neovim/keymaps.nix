df2f7cfec010421e505918fe0152f53b831bf285
Jann Stute <jann.stute@protonmail.com>
2026-06-15 00:58:42 +0200

    fix(vim): unbind arrow keys

--- common/programs/neovim/keymaps.nix
+++ common/programs/neovim/keymaps.nix
@@ -12,5 +12,25 @@
             key = ".";
             action = ":";
         }
+        {
+            mode = "n";
+            key = "<Up>";
+            action = "<Nop>";
+        }
+        {
+            mode = "n";
+            key = "<Down>";
+            action = "<Nop>";
+        }
+        {
+            mode = "n";
+            key = "<Left>";
+            action = "<Nop>";
+        }
+        {
+            mode = "n";
+            key = "<Right>";
+            action = "<Nop>";
+        }
     ];
 }
